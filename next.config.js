22040d8dcb7a5a43908f0579b81162ca6ecca5da
recflow <chjh9107@gmail.com>
2023-01-25 21:00:17 +0900

    practice

--- next.config.js
+++ next.config.js
@@ -1,6 +1,23 @@
 /** @type {import('next').NextConfig} */
+const API_KEY = process.env.APP_KEY;
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/contact/:path*",
+        destination: "/form/:path*",
+        permanent: false,
+      },
+    ];
+  },
+  async rewrites() {
+    return [{
+      source:"/api/movies",
+      destination:   `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+    }];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+import Seo from "./Seo";
+
+Head;
+export default function About() {
+  return (
+    <div>
+      <Seo title="About" />
+      <h1>About us</h1>
+    </div>
+  );
+}
